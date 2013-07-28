c609b5e6471d6f88429fb73aa5e00b186b6db0a5
jenofdoom <j_zajac@email.com>
2013-07-28 20:19:56 +1200

    Removed superflous line

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,6 @@
 todoList.controller('TodoCtrl', function TodoCtrl($scope, todoStorage) {
 
     $scope.todos = todoStorage.get();
-    $scope.newTodo = '';
 
     $scope.addTodo = function() {
         // this function gets called by the ng-submit directive on the new todo form
